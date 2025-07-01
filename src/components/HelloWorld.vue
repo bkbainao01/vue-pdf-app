ef15ec052b6897b26251d110840817158192e077
Sirimanee Pimkaew <sirimanee@imaxxsol.com>
2025-07-01 15:16:53 +0700

    update -pdf

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,10 +1,6 @@
 <template>
   <div class="p-4">
     <h2>Create PDF (Selectable Text)</h2>
-    <div class="mb-4">
-      <label>Name: <input v-model="form.name" class="border p-1" /></label><br />
-      <label>Age: <input v-model="form.age" type="number" class="border p-1" /></label>
-    </div>
     <button @click="generatePdf" class="bg-blue-500 text-white px-4 py-2 rounded">
       Download PDF
     </button>
@@ -18,44 +14,28 @@ import pdfFonts from 'pdfmake/build/vfs_fonts'
 
 // ✅ ใช้แบบนี้
 pdfMake.vfs = pdfFonts.vfs
-const form = reactive({
-  name: '',
-  age: ''
-})
 
 function generatePdf() {
-  const longText = 'This is a very long text that should wrap properly so that the table does not break and all rows are displayed completely. It helps test the table rendering in pdfMake with proper line wrapping.';
+  var docDefinition = {
+  content: [
+    {
+      layout: 'grid', // optional
+      table: {
+        // headers are automatically repeated if the table spans over multiple pages
+        // you can declare how many rows should be treated as headers
+        headerRows: 1,
+        widths: [ '*', 'auto', 100, '*' ],
 
-  const docDefinition = {
-    content: [
-      {
-        text: 'User Information',
-        style: 'header',
-        margin: [0, 0, 0, 0,  0]
-      },
-      {
-        table: {
-          headerRows: 1,
-          widths: ['auto', '*'],
-          body: [
-            ['Field', 'Value'],
-            ['Name', form.name],
-            ['Age', form.age.toString()],
-            ['Long Info', { text: longText, noWrap: false, margin: [0, 5, 0, 5] }]
-            ['1+Long Info', { text: longText, noWrap: false, margin: [0, 5, 0, 5] }]
-          ]
-        },
-        layout: 'lightHorizontalLines',
-        dontBreakRows: true,
-      },
-    ],
-    styles: {
-      header: {
-        fontSize: 18,
-        bold: true
+        body: [
+          [ 'First', 'Second', 'Third', 'The last one' ],
+          [ 'Value 1', 'Value 2', 'Value 3', 'Value 4' ],
+          [ { text: 'Bold value', bold: true }, 'Val 2', 'Val 3', 'Val 4' ]
+        ]
       }
     }
-  }
+  ]
+};
+
 
   pdfMake.createPdf(docDefinition).download();
 }
